fe94adcba335066a3d5f9a6589a8932cf19ff05a
nattoujam <Public.kyuuanago@gmail.com>
2024-04-07 00:52:56 +0900

    first commit

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,42 @@
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_ortho_4x4(
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,
+    KC_D,   KC_D,   KC_D,   KC_D,           KC_D,
+
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D, 
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,           KC_D, 
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D
+  )
+};
+/*
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  KC_D,   KC_NO,  KC_NO,
+
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D, 
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  KC_D, 
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  KC_NO,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  KC_NO
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,
+*/
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  KC_D,   KC_NO,  KC_NO,
+
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D, 
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  KC_D, 
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  KC_NO,
+    KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_D,   KC_NO,  KC_NO
